2fa0c7304ce67510fbdda7cb4e0155aebd9e5a5d
Aiden Cullo <aidencullo@gmail.com>
2023-05-08 10:31:35 -0500

    add PieceType test

--- src/models/PieceType.ts
+++ src/models/PieceType.ts
@@ -1,17 +1,48 @@
+/* 
+ * Type of chess piece
+ * 
+ * @author Aiden Cullo [https://github.com/aidencullo]
+ */
+
 export class PieceType {    
-    _name: String;
+    readonly _types: string[] = ["pawn", "knight", "bishop", "rook", "queen", "king"];
+    private _type: string;
     
-    constructor(name : String) {
-	this._name = name;
-    }
-    
-    get name(): String {
-	return this._name;
+    constructor(type : string) {
+	if (!this._types.includes(type)) {
+	    throw new Error("Error on PieceType construction, type parameter not an acceptable type")
+	}
+	this._type = type;
     }
 
-    set name(name: String) {
-	this._name = name;
+    promote(type: string) : void {
+	if (!this._types.includes(type)) {
+	    throw new Error("Error on PieceType promition, type parameter not an acceptable type")
+	}
+	this._type = type;
     }
 
+    isPawn() {
+	return this._type === "pawn";
+    }
+
+    isKnight() {
+	return this._type === "knight";
+    }
+
+    isBishop() {
+	return this._type === "bishop";
+    }
+
+    isRook() {
+	return this._type === "rook";
+    }
+
+    isQueen() {
+	return this._type === "queen";
+    }
+
+    isKing() {
+	return this._type === "king";
+    }
 }
-
---
+++ tests/models/PieceType.test.ts
@@ -0,0 +1,42 @@
+/*
+ * Unit test for PieceType object
+ *
+ * @author Aiden Cullo [https://github.com/aidencullo]
+ */
+
+import { PieceType } from '@models/PieceType';
+
+test("test constructors with acceptable string input", () => {
+    expect(() => new PieceType("pawn")).not.toThrowError();
+    expect(() => new PieceType("knight")).not.toThrowError();
+    expect(() => new PieceType("bishop")).not.toThrowError();
+    expect(() => new PieceType("rook")).not.toThrowError();
+    expect(() => new PieceType("queen")).not.toThrowError();
+    expect(() => new PieceType("king")).not.toThrowError();
+})
+
+test("test type value after constructor", () => {
+    const testPawn = new PieceType("pawn");
+    expect(testPawn.isPawn()).toBe(true);
+    const testKnight = new PieceType("knight");
+    expect(testKnight.isKnight()).toBe(true);
+    const testBishop = new PieceType("bishop");
+    expect(testBishop.isBishop()).toBe(true);
+    const testRook = new PieceType("rook");
+    expect(testRook.isRook()).toBe(true);
+    const testQueen = new PieceType("queen");
+    expect(testQueen.isQueen()).toBe(true);
+    const testKing = new PieceType("king");
+    expect(testKing.isKing()).toBe(true);
+    expect(testKing.isPawn()).toBe(false);
+    expect(testKing.isKnight()).toBe(false);
+    expect(testKing.isBishop()).toBe(false);
+    expect(testKing.isRook()).toBe(false);
+    expect(testKing.isQueen()).toBe(false);
+})
+
+test("constructor should throw error with incorrect value passed", () => {
+    expect(() => new PieceType("")).toThrowError();
+    expect(() => new PieceType("man")).toThrowError();
+    expect(() => new PieceType("KING")).toThrowError();
+})
