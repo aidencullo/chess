019adbf8c6e64e4453c7bbd7392d399c457b81ff
Aiden Cullo <aidencullo@gmail.com>
2023-05-09 11:01:11 -0500

    remove all logic from game comp -- will rewrite in board class

--- src/components/PawnComponent.tsx
+++ src/components/PawnComponent.tsx
@@ -2,9 +2,7 @@
 import React from 'react';
 
 // internal
-import { Highlight } from '@models/modular/Highlight';
 import { Pawn } from '@models/composite/pieces/Pawn';
-import { BOARD_WIDTH } from '@constants/board';
 import wpawn from '@/media/white/Pawn.png';
 import bpawn from '@/media/black/Pawn.png';
 
@@ -33,130 +31,7 @@ export default class PawnComponent extends React.Component<Props, State> {
 	this.state = {
 	    pawn: this.props.pawn,
 	};	
-	// this.handleClick = this.handleClick.bind(this);
     }
-
-      /************************************************************************/
-
-    /*
-     * HIGHLIGHTING
-     *
-     * highlight possible moves
-     */
-    
-    /************************************************************************/
-
-    // handleClick() {
-    // 	if (this.props.state.color === this.props.turn) {
-    // 	    this.highlight();
-    // 	}
-    // }
-
-    // highlight() {
-    // 	const highlights = this.props.highlights.slice();
-    // 	this.highlightMoves(this.props.index, highlights);
-    // 	this.highlightAttacks(this.props.index, highlights);
-    // 	this.highlightEnPassant(this.props.index, highlights);
-	
-    // 	this.props.setHighlights(highlights);
-    // }
-
-    highlightMoves(index : number, highlights : Highlight[]) {
-	let position = this.state.pawn.advanceOne();
-	
-	if (this.isValidMove(position)) {
-	    highlights[position].setOpen();
-	    position = this.state.pawn.advanceTwo();
-	    if (this.hasMoved(index)) {
-		return;
-	    }
-	    if (this.isValidMove(position)) {
-		highlights[position].setOpen();
-	    }
-	}
-    }
-
-    isValidMove(index : number) {
-	return this.isOnBoard(index) && !this.hasPiece(index);
-    }    
-
-    isOnBoard(index : number) {
-	return index > 0 && index < 63;
-    }   
- 
-    // onWestEdge(index : number) {
-    // 	return column(index) === 0;
-    // }    
-
-    // onEastEdge(index : number) {
-    // 	return column(index) === BOARD_WIDTH - 1;
-    // }    
-
-    hasMoved(index : number) {
-	if (this.state.pawn.isWhite()) {
-	    return Math.floor(index / BOARD_WIDTH) !== 6;
-	}
-	return Math.floor(index / BOARD_WIDTH) !== 1;
-    }
-
-    hasPiece(index : number) {
-	return index > 0;
-	// return this.props.squares[index].piece.isNoPiece();
-    }    
-	
-    // highlightAttacks(currentIndex, highlights) {
-    // 	let pawn = this.props.squares[currentIndex];
-    // 	let direction = getDirection(pawn.color);
-    // 	let targetIndex = currentIndex + (direction * BOARD_WIDTH) + 1;
-
-    // 	if (!this.onEastEdge(currentIndex)) {
-    // 	    highlights[targetIndex] = Number(this.hasPiece(targetIndex) * 2);	    
-    // 	}
-
-    // 	targetIndex = currentIndex + (direction * BOARD_WIDTH) - 1;
-	
-    // 	if (!this.onWestEdge(currentIndex)) {
-    // 	    highlights[targetIndex] = Number(this.hasPiece(targetIndex) * 2);	    
-    // 	}
-
-    // }
-
-    // highlightEnPassant(index, highlights) {
-    // 	let pawn = this.props.squares[index];
-    // 	let direction = getDirection(pawn.color);
-	
-    // 	// let targetMoveEast : MoveI = {
-    // 	//     start: index + (direction * 2 * BOARD_WIDTH) + 1,
-    // 	//     end: index + 1,
-    // 	//     piece: {
-    // 	// 	color: pawn.color === Color.White ? Color.Black : Color.White,
-    // 	// 	piece: Piece.PawnComponent
-    // 	//     } as Piece
-    // 	// };
-
-    // 	// let targetMoveWest : MoveI = {
-    // 	//     start: index + (direction * 2 * BOARD_WIDTH) - 1,
-    // 	//     end: index - 1,
-    // 	//     piece: {
-    // 	// 	color: pawn.color === Color.White ? Color.Black : Color.White,
-    // 	// 	piece: Piece.PawnComponent
-    // 	//     } as Piece
-    // 	// };
-
-    // 	let targetMoveEast : MoveI = {};
-
-    // 	let targetMoveWest : MoveI = {};
-
-    // 	if (this.props.lastMove.isEqual(targetMoveEast)) {
-    // 	    let position = index + (direction * BOARD_WIDTH) + 1;
-    // 	    highlights[position] = 2;
-    // 	    return;
-    // 	}
-    // 	if (this.props.lastMove.isEqual(targetMoveWest)) {
-    // 	    let position = index + (direction * BOARD_WIDTH) - 1;
-    // 	    highlights[position] = 2;
-    // 	}
-    // }
     
     render() {
 	return (
--- src/components/SquareComponent.tsx
+++ src/components/SquareComponent.tsx
@@ -5,24 +5,35 @@ import React from 'react';
 import { Pawn } from '@models/composite/pieces/Pawn';
 import PawnComponent from '@components/PawnComponent';
 import { BOARD_WIDTH } from '@constants/board';
-import { Move } from '@models/composite/Move';
 import { Square } from '@models/composite/Square';
 import { Highlight } from '@models/modular/Highlight';
 
 type Props = {
     key : number;
-    index : number;
-    highlights : Highlight[];
-    setHighlights : (h : Highlight[]) => void;
-    squares : Square[];
-    lastMove : Move | null;
+    highlight : Highlight;
+    square : Square;
 }
 
-export default class SquareComponent extends React.Component<Props> {
+type State = {
+    index : number;
+    highlight : Highlight;
+    square : Square;
+}
 
+export default class SquareComponent extends React.Component<Props, State> {
+
+    constructor(props : Props) {
+	super(props);
+	this.state = {
+	    highlight : this.props.highlight,
+	    index : this.props.key,
+	    square : this.props.square,
+	}
+    }
+    
     getColor() {
-	let index = this.props.index;
-	let row = Math.floor(index / BOARD_WIDTH);
+	const index = this.state.index;
+	const row = Math.floor(index / BOARD_WIDTH);
 	if (row % 2 !== 0) {
 	    return index % 2 === 0 ? "brown" : "white";
 	} else {
@@ -30,7 +41,8 @@ export default class SquareComponent extends React.Component<Props> {
 	}
     }
 
-    getHighlight(highlight : Highlight) {
+    getHighlight() {
+	const highlight = this.state.highlight;
 	if (highlight.isOpen() || highlight.isEnPassant()) {
 	    return "green";
 	} else if (highlight.isAttack()) {
@@ -40,71 +52,22 @@ export default class SquareComponent extends React.Component<Props> {
 	}
     }
 
-    // render() {
-    // 	return (
-    // 	    <button
-    // 		className="square"
-    // 		style={{ 
-    // 		    backgroundColor: this.getColor(),
-    // 		    borderColor: this.setColor(this.props.highlight),
-    // 		}}
-    // 		onClick={() => this.props.handleClick()}
-    // 	    >
-    // 		{(() => {
-    // 		    switch (this.props.state.piece) {
-    // 		    case Piece.Pawn:
-    // 			return <Pawn
-    // 				   state={this.props.state}
-    // 				   highlights={this.props.highlights}
-    // 				   setHighlights={this.props.setHighlights}
-    // 				   index={this.props.index}
-    // 				   squares={this.props.squares}				   
-    // 				   turn={this.props.turn}
-    // 				   lastMove={this.props.lastMove}
-    // 			       />;
-    // 		    case Piece.Knight:
-    // 			return <Knight
-    // 				   state={this.props.state}
-    // 			       />;
-    // 		    case Piece.BishopComponent:
-    // 			return <BishopComponent
-    // 				   state={this.props.state}
-    // 			       />;
-    // 		    case Piece.Rook:
-    // 			return <Rook
-    // 				   state={this.props.state}
-    // 			       />;
-    // 		    case Piece.Queen:
-    // 			return <Queen
-    // 				   state={this.props.state}
-    // 			       />;
-    // 		    case Piece.KingComponent:
-    // 			return <KingComponent
-    // 				   state={this.props.state}
-    // 			       />;
-    // 		    default:
-    // 		    }
-    // 		})()}		
-    // 	    </button>
-    // 	);
-    // }
-
     render() {
 	return (
 	    <button
 	    className="square"
 	    style={{ 
 		backgroundColor: this.getColor(),
-		borderColor: this.getHighlight(this.props.highlights[this.props.index]),
-	    }}
-		>
+		borderColor: this.getHighlight(),
+	    }}>
 
 		{ 
-		    this.props.squares[this.props.index].getPiece() &&
+		    this.state.square.getPiece() &&
 			<PawnComponent
-		    pawn={this.props.squares[this.props.index].getPiece() as Pawn}
+		    pawn={this.state.square.getPiece() as Pawn}
 			/>
 		}
+
 	    </button>
 	);
     }
