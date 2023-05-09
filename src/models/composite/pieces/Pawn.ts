1b2de7d13a3139d961241a2aba8c707ac9b1006f
Aiden Cullo <aidencullo@gmail.com>
2023-05-08 19:22:45 -0500

    finished pawn test

--- src/models/composite/pieces/Pawn.ts
+++ src/models/composite/pieces/Pawn.ts
@@ -4,44 +4,12 @@
  * @author Aiden Cullo [https://github.com/aidencullo]
  */
 
-import { PieceType } from '@models/modular/PieceType';
-import { Color } from '@models/modular/Color';
+import { Piece } from '@models/composite/Piece';
 
-export class Piece {    
-    private _color: Color;
-    private _pieceType: PieceType;
+export class Pawn extends Piece {    
     
-    constructor(pieceType : string, color : string) {
-	this._color = new Color(color);
-	this._pieceType = new PieceType(pieceType);
-    }
-
-    isWhite() {
-	return this._color.isWhite();
-    }
-
-    isPawn() {
-	return this._pieceType.isPawn();
-    }
-
-    isKnight() {
-	return this._pieceType.isKnight();
-    }
-
-    isBishop() {
-	return this._pieceType.isBishop();
-    }
-
-    isRook() {
-	return this._pieceType.isRook();
-    }
-
-    isQueen() {
-	return this._pieceType.isQueen();
-    }
-
-    isKing() {
-	return this._pieceType.isKing();
+    constructor(color : string) {
+	super("pawn", color)
     }
     
 }
--- tests/models/composite/pieces/Pawn.test.ts
+++ tests/models/composite/pieces/Pawn.test.ts
@@ -8,45 +8,27 @@ import { Pawn } from '@models/composite/pieces/Pawn';
 
 test("test constructors with acceptable string input", () => {
 
-    expect(() => new Pawn("pawn", "white")).not.toThrowError();
-    expect(() => new Pawn("pawn", "black")).not.toThrowError();
-    expect(() => new Pawn("knight", "white")).not.toThrowError();
-    expect(() => new Pawn("bishop", "white")).not.toThrowError();
-    expect(() => new Pawn("rook", "white")).not.toThrowError();
-    expect(() => new Pawn("queen", "white")).not.toThrowError();
-    expect(() => new Pawn("king", "white")).not.toThrowError();
+    expect(() => new Pawn("white")).not.toThrowError();
+    expect(() => new Pawn("black")).not.toThrowError();
     
 })
 
 test("test type value after constructor", () => {
+
+    const pawn = new Pawn("white");
     
-    const testWhitePawn = new Pawn("pawn", "white");
-    expect(testWhitePawn.isWhite()).toBe(true);
-    expect(testWhitePawn.isPawn()).toBe(true);
-    expect(testWhitePawn.isBishop()).toBe(false);
-    expect(testWhitePawn.isKnight()).toBe(false);
-    expect(testWhitePawn.isRook()).toBe(false);
-    expect(testWhitePawn.isQueen()).toBe(false);
-    expect(testWhitePawn.isKing()).toBe(false);
-    
-    const testBlackPawn = new Pawn("pawn", "black");
-    expect(testBlackPawn.isWhite()).toBe(false);
-    expect(testBlackPawn.isPawn()).toBe(true);
-    expect(testBlackPawn.isKing()).toBe(false);
+    expect(pawn.isPawn()).toBe(true);    
+    expect(pawn.isKing()).toBe(false);    
+    expect(pawn.isBishop()).toBe(false);    
+    expect(pawn.isWhite()).toBe(true);    
     
 })
 
 test("constructor should throw error with incorrect value passed", () => {
     
-    expect(() => new Pawn("","")).toThrowError();
-    expect(() => new Pawn("pawn","")).toThrowError();
-    expect(() => new Pawn("pawn","red")).toThrowError();
-    expect(() => new Pawn("knight","red")).toThrowError();
-    expect(() => new Pawn("king","WHITE")).toThrowError();
-    expect(() => new Pawn("person","white")).toThrowError();
-    expect(() => new Pawn("white","WHITE")).toThrowError();
-    expect(() => new Pawn("pawn","w")).toThrowError();
-    expect(() => new Pawn("bishop","")).toThrowError();
-    expect(() => new Pawn("rook","    ")).toThrowError();
+    expect(() => new Pawn("green")).toThrowError();
+    expect(() => new Pawn("")).toThrowError();
+    expect(() => new Pawn("jlskadf")).toThrowError();
+    expect(() => new Pawn("whit")).toThrowError();
     
 })
